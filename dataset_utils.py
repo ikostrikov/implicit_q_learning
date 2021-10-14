1dd5e3ee5d9d5b177f171da6762877ebd3533f7e
Ashvin Nair <nair.ashvin@gmail.com>
2021-10-14 11:18:40 -0700

    Add finetuning experiment. Add option for no lr decay.

--- dataset_utils.py
+++ dataset_utils.py
@@ -5,6 +5,8 @@ import gym
 import numpy as np
 from tqdm import tqdm
 
+from typing import Optional
+
 Batch = collections.namedtuple(
     'Batch',
     ['observations', 'actions', 'rewards', 'masks', 'next_observations'])
@@ -98,3 +100,71 @@ class D4RLDataset(Dataset):
                          next_observations=dataset['next_observations'].astype(
                              np.float32),
                          size=len(dataset['observations']))
+
+
+class ReplayBuffer(Dataset):
+    def __init__(self, observation_space: gym.spaces.Box, action_dim: int,
+                 capacity: int):
+
+        observations = np.empty((capacity, *observation_space.shape),
+                                dtype=observation_space.dtype)
+        actions = np.empty((capacity, action_dim), dtype=np.float32)
+        rewards = np.empty((capacity, ), dtype=np.float32)
+        masks = np.empty((capacity, ), dtype=np.float32)
+        dones_float = np.empty((capacity, ), dtype=np.float32)
+        next_observations = np.empty((capacity, *observation_space.shape),
+                                     dtype=observation_space.dtype)
+        super().__init__(observations=observations,
+                         actions=actions,
+                         rewards=rewards,
+                         masks=masks,
+                         dones_float=dones_float,
+                         next_observations=next_observations,
+                         size=0)
+
+        self.size = 0
+
+        self.insert_index = 0
+        self.capacity = capacity
+
+    def initialize_with_dataset(self, dataset: Dataset,
+                                num_samples: Optional[int]):
+        assert self.insert_index == 0, 'Can insert a batch online in an empty replay buffer.'
+
+        dataset_size = len(dataset.observations)
+
+        if num_samples is None:
+            num_samples = dataset_size
+        else:
+            num_samples = min(dataset_size, num_samples)
+        assert self.capacity >= num_samples, 'Dataset cannot be larger than the replay buffer capacity.'
+
+        if num_samples < dataset_size:
+            perm = np.random.permutation(dataset_size)
+            indices = perm[:num_samples]
+        else:
+            indices = np.arange(num_samples)
+
+        self.observations[:num_samples] = dataset.observations[indices]
+        self.actions[:num_samples] = dataset.actions[indices]
+        self.rewards[:num_samples] = dataset.rewards[indices]
+        self.masks[:num_samples] = dataset.masks[indices]
+        self.dones_float[:num_samples] = dataset.dones_float[indices]
+        self.next_observations[:num_samples] = dataset.next_observations[
+            indices]
+
+        self.insert_index = num_samples
+        self.size = num_samples
+
+    def insert(self, observation: np.ndarray, action: np.ndarray,
+               reward: float, mask: float, done_float: float,
+               next_observation: np.ndarray):
+        self.observations[self.insert_index] = observation
+        self.actions[self.insert_index] = action
+        self.rewards[self.insert_index] = reward
+        self.masks[self.insert_index] = mask
+        self.dones_float[self.insert_index] = done_float
+        self.next_observations[self.insert_index] = next_observation
+
+        self.insert_index = (self.insert_index + 1) % self.capacity
+        self.size = min(self.size + 1, self.capacity)
